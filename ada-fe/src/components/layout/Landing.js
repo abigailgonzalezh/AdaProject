eb1802c05849ac1dea115f87c255b303cc3efa04
Alkiva <fernanda22mr@gmail.com>
2019-11-07 22:12:45 -0600

    buttons

--- ada-fe/src/components/layout/Landing.js
+++ ada-fe/src/components/layout/Landing.js
@@ -6,6 +6,7 @@ import purple from '@material-ui/core/colors/purple';
 import Contact from '@material-ui/icons/ChildCareOutlined';
 import Eye from '@material-ui/icons/VisibilityOutlined';
 import Heart from '@material-ui/icons/FavoriteBorder';
+import Fab from '@material-ui/core/Fab';
 
 const accent = purple['A200'];
 const useStyles = makeStyles(theme => ({
@@ -19,27 +20,37 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
 export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: '#f8bbd0', color: 'black'}}>
+          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: '#ffe0eb', color: 'black'}}>
             <br/>
             <br/>
             <br/>
             <br/>
-            <div><Contact/>Avisa a tus contactos</div>
+            <div><Contact/>  Avisa a tus contactos</div>
             <br/>
-            <div><Eye/>Que alguien sepa de ti</div>
+            <div><Eye/>  Que alguien sepa de ti</div>
             <br/>
-            <div><Heart/>Sal sin miedo </div>
+            <div><Heart/>  Sal sin miedo </div>
           </Grid>
 
 
-          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: 'white', color: 'black'}}>
-            adios
+          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: '#fffcfd', color: 'black'}}>
+          <br/>
+            <br/>
+            <br/>
+            <br/>
+            <Fab variant="rounded"  className={classes.fab} style={{backgroundColor: '#f09eba'}}>Iniciar sesion</Fab>
+            <br/>
+            <br/>
+            <Fab variant="rounded"  className={classes.fab} style={{backgroundColor: '#f09eba'}}>Registrarse</Fab>
+            <br/>
+
           </Grid>
 
       </Grid>
