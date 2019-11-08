93f6088b29006668bfc87a1f0e12a726b1a4241e
Alkiva <fernanda22mr@gmail.com>
2019-11-07 21:43:50 -0600

    icons

--- ada-fe/src/components/layout/Landing.js
+++ ada-fe/src/components/layout/Landing.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import purple from '@material-ui/core/colors/purple';
+import Contact from '@material-ui/icons/ChildCareOutlined';
+import Eye from '@material-ui/icons/VisibilityOutlined';
+import Heart from '@material-ui/icons/FavoriteBorder';
 
+const accent = purple['A200'];
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -20,12 +25,20 @@ export default function FullWidthGrid() {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-
-          <Grid item xs={12} sm={6}>
-            hola
+          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: '#f8bbd0', color: 'black'}}>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div><Contact/>ContactlessiconAvisa a tus contactos</div>
+            <br/>
+            <div><Eye/>Que alguien sepa de ti</div>
+            <br/>
+            <div><Heart/>Sal sin miedo </div>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+
+          <Grid justify="flex-start" item xs={12} lg={6} style={{backgroundColor: 'white', color: 'black'}}>
             adios
           </Grid>
 
