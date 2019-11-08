ae4483d90fcfdf3328f7777d0763c0aa1707222e
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-11-07 19:49:55 -0600

    se creo la landing page (provisional)

---
+++ ada-fe/src/components/layout/Landing.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+    render() {
+        return <h1>Hola soy la Landing Page!</h1>;
+      }
+}
+
+export default Landing;
