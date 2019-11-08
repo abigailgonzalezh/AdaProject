12965660a9274d336aa31c66c7a1ee13784f72fe
Alkiva <fernanda22mr@gmail.com>
2019-11-07 20:12:52 -0600

    se añadio grid

--- ada-fe/src/components/layout/Landing.js
+++ ada-fe/src/components/layout/Landing.js
@@ -1,9 +1,35 @@
-import React, { Component } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-class Landing extends Component {
-    render() {
-        return <h1>Hola soy la Landing Page!</h1>;
-      }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+
+          <Grid item xs={12} sm={6}>
+            hola
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            adios
+          </Grid>
+
+      </Grid>
+    </div>
+  );
 }
-
-export default Landing;
