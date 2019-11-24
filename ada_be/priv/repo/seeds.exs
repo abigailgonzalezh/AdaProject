c28a1417d6fcfc72971f6ae075fc010a713acc62
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-11-24 16:36:56 -0600

    agregue usuarios prueba al seeds

--- ada_be/priv/repo/seeds.exs
+++ ada_be/priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias AdaBe.{Repo, Accounts.User}
+
+[
+    %{username: "admin", email: "admin@admin.com", password: "1234"},
+    %{username: "abi", email: "abi@abi.com", password: "1234"},
+    %{username: "fer", email: "fer@abi.com", password: "1234"}
+] |> Enum.each(fn user ->
+      User.changeset(%User{}, user)
+      |> Repo.insert!
+    end)
