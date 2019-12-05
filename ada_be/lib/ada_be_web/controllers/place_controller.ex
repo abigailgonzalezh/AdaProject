ef6189a125a26357d8001b7a3e641eb7743fa6c1
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-12-05 14:47:08 -0600

    place controller

--- ada_be/lib/ada_be_web/controllers/place_controller.ex
+++ ada_be/lib/ada_be_web/controllers/place_controller.ex
@@ -1,5 +1,9 @@
 defmodule AdaBeWeb.PlaceController do
     use AdaBeWeb, :controller
 
-    
+    def index(conn, _params) do
+      IO.puts "---------------------------------------------"
+      IO.inspect user = AuthBeWeb.Guardian.Plug.current_resource(conn)
+      json(conn, %{msg: "Hello #{user.username}"})
+    end
 end
