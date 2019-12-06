b291e252bd76326bc7e2da21805c597ac7aefae4
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-12-06 15:20:03 -0600

    ya esta place controller

--- ada_be/lib/ada_be_web/controllers/place_controller.ex
+++ ada_be/lib/ada_be_web/controllers/place_controller.ex
@@ -1,21 +1,24 @@
 defmodule AdaBeWeb.PlaceController do
     use AdaBeWeb, :controller
+    import Ecto.Query, only: [from: 2]
 
     alias AdaBe.Menu.Place
+    alias AdaBe.Accounts.User
+    alias AdaBe.Repo
 
     def index(conn, _params) do
       IO.puts "---------------------------------------------"
-      IO.inspect place = AdaBeWeb.Guardian.Plug.current_resource(conn)
-      json(conn, %{msg: "Hello #{place.name}"})
+      IO.inspect user = AdaBeWeb.Guardian.Plug.current_resource(conn)
+
+      names = Repo.all(from p in Place, where: p.user_id == ^user.id, select: p.name)
+
+      json(conn, %{msg: "Hello", names: names})
     end
 
     def create(conn, %{"place" => place_params}) do
-        
-    end
-
-    def show(conn, %{"id" => id}) do
-        IO.puts "---------------------------------------------"
-        IO.inspect individual_place = AdaBeWeb.Guardian.Plug.current_resource(conn)
-        json(conn, %{msg: "Aqui estoy"})
+        AdaBeWeb.Guardian.Plug.current_resource(conn)
+        |> Ecto.build_assoc(:places, params)
+        |> Repo.insert!(place)
+        json(conn, %{msg: "Place successfully registered"})
     end
 end
--- ada_be/lib/ada_be_web/router.ex
+++ ada_be/lib/ada_be_web/router.ex
@@ -19,10 +19,9 @@ defmodule AdaBeWeb.Router do
   end
  
    scope "/api", AdaBeWeb do
-    pipe_through :api
+    pipe_through [:api, :authenticated]
 
     get "/places", PlaceController, :index #renders a list of all items of the given resource type
     post "/places", PlaceController, :create #receives params for one new item and saves it in a datastore
-    get "/places/:id", PlaceController, :show #renders an individual item by id
    end
 end
