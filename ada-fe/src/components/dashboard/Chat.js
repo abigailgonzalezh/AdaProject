7ed9b7c02463565dab06666d40f3553b21ec5a3b
Alkiva <fernanda22mr@gmail.com>
2019-12-07 17:43:10 -0600

    route chat

--- ada-fe/src/components/dashboard/Chat.js
+++ ada-fe/src/components/dashboard/Chat.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import Account from '@material-ui/icons/AccountCircle';
-import { BrowserRouter as Link, } from "react-router-dom";
+import { BrowserRouter as Router, Route , Link, } from "react-router-dom";
 import Container from '@material-ui/core/Container';
 class Review extends Component {
   constructor(props) {
